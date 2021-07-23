0aa3097e1eaaabb03c03f4b3bdbbd4eab78c0d99
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-07-24 00:21:25 +0530

    uselayout hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useLayoutEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    console.log('use effect lifecycle : componentDidMount');
+  }, [])
+
+  useLayoutEffect(() => {
+    console.log('use layout effect lifecycle : componentDidMount');
+  }, [])
+
+  console.log('render lifecycle');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label>Your name</label>
+        <input type='text' autoComplete='off'/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// In console,
+// Before the return statement I’ve added a couple console messages to see the order of those messages.
+
+// useLayoutEffect and useEffect are only being executed after a component did mount lifeycle.
